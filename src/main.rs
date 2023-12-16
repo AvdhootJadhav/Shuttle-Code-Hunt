388da0b79affe25ca8efdbb67e12c4a7a51e67b1
AvdhootJadhav <avdhootjadhav002@gmail.com>
2023-12-16 14:46:39 +0530

    day 1 complete

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
-use rocket::{get, routes};
+use std::path::PathBuf;
+
+use rocket::data::FromData;
+use rocket::{
+    get, post, routes,
+    serde::{self, json::Json, Deserialize},
+};
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(crate = "rocket::serde")]
+struct Reindeer {
+    name: String,
+    strength: i32,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(crate = "rocket::serde")]
+struct Data(Vec<Reindeer>);
+
+#[post("/4/strength", data = "<data>")]
+fn calculate_strength(data: Json<Data>) -> String {
+    let Json(data) = data;
+    let Data(data) = data;
+
+    let res = data
+        .iter()
+        .map(|data| data.strength)
+        .sum::<i32>()
+        .to_string();
+
+    return res;
 }
 
 #[get("/1/<path..>")]
@@ -13,8 +44,7 @@ fn cube_the_bits(path: PathBuf) -> String {
         let value = num.to_str().unwrap().parse::<i32>().unwrap();
         if calculated_value == -1 {
             calculated_value = value;
-        }
-        else {
+        } else {
             calculated_value ^= value;
         }
     }
@@ -24,7 +54,7 @@ fn cube_the_bits(path: PathBuf) -> String {
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index]);
+    let rocket = rocket::build().mount("/", routes![index, cube_the_bits, calculate_strength]);
 
     Ok(rocket.into())
 }
